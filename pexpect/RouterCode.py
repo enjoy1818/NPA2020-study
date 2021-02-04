f737c6e46b97821ab4e732bf44f3c31d81484f79
enjoy1818 <60070092@kmitl.ac.th>
2021-02-05 02:43:05 +0700

    Add RouterCode Unit testing

--- pexpect/RouterCode.py
+++ pexpect/RouterCode.py
@@ -1,4 +1,5 @@
 import netmiko
+import unittest
 
 class Router:
     """Represent router in environment"""
@@ -10,31 +11,29 @@ class Router:
         self.vendor = vendor
         self.interface = []
         self.neighbor = {}
-
     def add_interface(self, interface):
         """Add interface to interface dictionary"""
         self.interface.append(interface)
     
-    def list_interface(self):
+    def interface_list(self):
         """Listing interface of router"""
         return self.interface
 
     def neigbors_list(self):
         """Listing neighbor of router"""
         return self.neighbor
-
     def validate_interface(self, interface):
         """validate interface before take any action"""
         return interface in self.interface
-
     def connect_to(self, localInterface, remoteInterface, remoteHost):
         """Connect routers"""
         if self.validate_interface(localInterface):
             self.add_neighbor(neighborName=remoteHost, localInterface=localInterface, remoteInterface=remoteInterface)
+            return True
         else:
-            print("Error : Invalid interface")
-            
+            return False
     def add_neighbor(self, neighborName, localInterface, remoteInterface):
+        """Neighbor"""
         if self.validate_interface(localInterface):
             if neighborName not in self.neighbor:
                 self.neighbor[neighborName] = [{localInterface:remoteInterface}]
@@ -42,30 +41,32 @@ class Router:
                 self.neighbor[neighborName].append({localInterface:remoteInterface})
         else:
             print("Error: Invalid interface")
+    def remove_neighbor(self, neighborName, localInterface, remoteInterface):
+        """Removing connected neighbor"""
             
             
 
 def main():
     """Main logic"""
-    router_1 = Router("1.1.1.1", "router1", "cisco")
-    router_1.add_interface("G1/0/1")
-    router_1.add_interface("G1/0/2")
-    router_2 = Router("2.2.2.2", "router2", "cisco")
-    router_2.add_interface("G1/0/1")
+    # router_1 = Router("1.1.1.1", "router1", "cisco")
+    # router_1.add_interface("G1/0/1")
+    # router_1.add_interface("G1/0/2")
+    # router_2 = Router("2.2.2.2", "router2", "cisco")
+    # router_2.add_interface("G1/0/1")
 
-    print(router_1.list_interface())
+    # # print(router_1.interface_list())
 
-    connect("G1/0/2", router_1, "G1/0/1", router_2)
+    # connect("G1/0/2", router_1, "G1/0/1", router_2)
 
-    print(router_1.neigbors_list())
-    print(router_2.neigbors_list())
+    # # print(router_1.neigbors_list())
+    # # print(router_2.neigbors_list())
 
 
 def connect(localInterface, localhost, remoteInterface, remoteHost):
 
-    localhost.connect_to(localInterface, remoteInterface, remoteHost.hostName)
-    remoteHost.connect_to(remoteInterface, localInterface, localhost.hostName)
-
+    res1 = localhost.connect_to(localInterface, remoteInterface, remoteHost.hostName)
+    res2 = remoteHost.connect_to(remoteInterface, localInterface, localhost.hostName)
+    return res1 and res2
 
 main()
     
---
+++ pexpect/Week3-TDD-test.py
@@ -0,0 +1,33 @@
+import unittest
+from RouterCode import *
+# from RouterCode import Router
+
+class Router_Test(unittest.TestCase):
+    def test_interface_list(self):
+        router = Router('1.1.1.1', 'Router-1', 'cisco')
+        router.add_interface("G1/0/1")
+        # print(router.interface_list())
+        self.assertEqual(router.interface_list(), ['G1/0/1'])
+    def test_validate_interface(self):
+        router = Router('1.1.1.1', 'Router-1', 'cisco')
+        router.add_interface("G1/0/1")
+        self.assertTrue(router.validate_interface('G1/0/1'))
+        self.assertFalse(router.validate_interface('G1/0/2'))
+    def test_connect(self):
+        router_1 = Router('1.1.1.1', 'Router-1', 'cisco')
+        router_2 = Router('2.2.2.2.', "Router-2", "cisco")
+        router_1.add_interface("G1/0/1")
+        router_2.add_interface("G1/0/2")
+        self.assertFalse(connect("G1/0/2", router_1, "G1/0/1", router_2))
+        self.assertTrue(connect("G1/0/1", router_1, "G1/0/2", router_2))
+    def test_neighbor_list(self):
+        router_1 = Router('1.1.1.1', 'Router-1', 'cisco')
+        router_2 = Router('2.2.2.2.', "Router-2", "cisco")
+        router_1.add_interface("G1/0/1")
+        router_2.add_interface("G1/0/2")
+        self.assertDictEqual(router_1.neigbors_list(), {})
+        connect("G1/0/1", router_1, "G1/0/2", router_2)
+        expectedResult = {'Router-2':[{'G1/0/1':'G1/0/2'}]}
+        self.assertDictEqual(router_1.neigbors_list(), expectedResult)
+if __name__=='__main__':
+    unittest.main()
